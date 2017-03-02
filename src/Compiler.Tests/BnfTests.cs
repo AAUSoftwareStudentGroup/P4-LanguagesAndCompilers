df14bb5f2b41887c85f55acae814c9797231bd86
Mathias Ibsen <mathias@ibsen.name>
2017-03-02 23:28:21 +0100

    Add test for a ll2 grammar

--- src/Compiler.Tests/BnfTests.cs
+++ src/Compiler.Tests/BnfTests.cs
@@ -2,116 +2,148 @@
 using System.Collections.Generic;
 using Xunit;
 using System.Linq;
+using System;
 
 namespace Compiler.Tests
 {
-    public class BnfTests
-    {
-        Symbol TestSymbol(string name) => new Symbol()
-        {
-            Name = name
-        };
+	public class BnfTests
+	{
+		Symbol TestSymbol(string name) => new Symbol()
+		{
+			Name = name
+		};
 
-        Expansion TestExpansion(params Symbol[] symbols) => new Expansion()
-        {
-            Symbols = symbols.ToList()
-        };
+		Expansion TestExpansion(params Symbol[] symbols) => new Expansion()
+		{
+			Symbols = symbols.ToList()
+		};
 
-        Production TestProduction(string productionName, params Expansion[] expansions) => new Production()
-        {
-            Name = productionName,
-            Expansions = expansions.ToList()
-        };
+		Production TestProduction(string productionName, params Expansion[] expansions) => new Production()
+		{
+			Name = productionName,
+			Expansions = expansions.ToList()
+		};
 
-        Bnf TestLL1Bnf()
-        {
-            Symbol a = TestSymbol("a");
-            Symbol b = TestSymbol("b");
-            Symbol d = TestSymbol("d");
-            Symbol e = TestSymbol("EPSILON");
-            
-            Production D = TestProduction("D", TestExpansion(d),
-                                               TestExpansion(e));
-            Production B = TestProduction("B", TestExpansion(b),
-                                               TestExpansion(e));
-            Production A = TestProduction("A", TestExpansion(B, D));
-            Production S = TestProduction("S", TestExpansion(A, a));
+		Bnf TestLL1Bnf()
+		{
+			Symbol a = TestSymbol("a");
+			Symbol b = TestSymbol("b");
+			Symbol d = TestSymbol("d");
+			Symbol e = TestSymbol("EPSILON");
 
-            List<Production> productions = new List<Production>() { D, B, A, S };
+			Production D = TestProduction("D", TestExpansion(d),
+											   TestExpansion(e));
+			Production B = TestProduction("B", TestExpansion(b),
+											   TestExpansion(e));
+			Production A = TestProduction("A", TestExpansion(B, D));
+			Production S = TestProduction("S", TestExpansion(A, a));
 
-            return new Bnf(S, productions, new List<Symbol>() { a, b, d, e });
-        }
+			List<Production> productions = new List<Production>() { D, B, A, S };
 
-        Bnf TestLL1Bnf2()
-        {
-            Symbol a = TestSymbol("a");
-            Symbol b = TestSymbol("b");
-            Symbol d = TestSymbol("d");
-            Symbol e = TestSymbol("EPSILON");
+			return new Bnf(S, productions, new List<Symbol>() { a, b, d, e });
+		}
 
-            Production D = TestProduction("D", TestExpansion(d),
-                                               TestExpansion(e));
-            Production B = TestProduction("B", TestExpansion(b));
-            Production A = TestProduction("A", TestExpansion(B, D));
-            Production S = TestProduction("S", TestExpansion(A, a));
+		Bnf TestLL1Bnf2()
+		{
+			Symbol a = TestSymbol("a");
+			Symbol b = TestSymbol("b");
+			Symbol d = TestSymbol("d");
+			Symbol e = TestSymbol("EPSILON");
 
-            List<Production> productions = new List<Production>() { D, B, A, S };
+			Production D = TestProduction("D", TestExpansion(d),
+											   TestExpansion(e));
+			Production B = TestProduction("B", TestExpansion(b));
+			Production A = TestProduction("A", TestExpansion(B, D));
+			Production S = TestProduction("S", TestExpansion(A, a));
 
-            return new Bnf(S, productions, new List<Symbol>() { a, b, d, e });
-        }
+			List<Production> productions = new List<Production>() { D, B, A, S };
 
-        [Fact]
-        public void Constructor()
-        {
-            Bnf bnf = TestLL1Bnf();
+			return new Bnf(S, productions, new List<Symbol>() { a, b, d, e });
+		}
 
-            Assert.NotNull(bnf);
-        }
+		/* S -> f S Y
+		 * | EPSILON
+		 * Y -> f g S 
+		 * | y */
+		Bnf TestLL2Bnf()
+		{
+			Symbol f = TestSymbol("f");
+			Symbol g = TestSymbol("g");
+			Symbol y = TestSymbol("y");
+			Symbol e = TestSymbol("EPSILON");
 
-        [Fact]
-        public void IsLL1()
-        {
-            Bnf bnf = TestLL1Bnf();
+			Production Y = TestProduction("Y", TestExpansion(f, g), TestExpansion(y));
+			Production S = TestProduction("S", TestExpansion(f), TestExpansion(e));
+			// add non-terminals S, Y to S
+			S.Expansions[0].Symbols.Add(S);
+			S.Expansions[0].Symbols.Add(Y);
+			// add non-terminal S to Y
+			Y.Expansions[0].Symbols.Add(S);
+			List<Production> productions = new List<Production>() { Y, S };
 
-            string error;
-            Assert.True(bnf.IsLL1(out error));
-        }
+			return new Bnf(S, productions, new List<Symbol>() { f, g, y, e }); ;
+		}
 
-        [Fact]
-        public void FirstSet()
-        {
-            Bnf bnf = TestLL1Bnf();
-            Production S = bnf.Productions.FirstOrDefault(p => p.Name == "S");
-            Symbol a = bnf.Terminals.FirstOrDefault(s => s.Name == "a");
-            Symbol b = bnf.Terminals.FirstOrDefault(s => s.Name == "b");
-            Symbol d = bnf.Terminals.FirstOrDefault(s => s.Name == "d");
-            List<Symbol> expected = new List<Symbol>() { a, b, d };
+		[Fact]
+		public void Constructor()
+		{
+			Bnf bnf = TestLL1Bnf();
 
-            foreach (var item in S.FirstSet())
-            {
-                System.Diagnostics.Debug.WriteLine("expected" + " " + item);
-            }
+			Assert.NotNull(bnf);
+		}
 
-            Assert.Equal(expected.Count, S.FirstSet().Intersect(expected).Count());
+		[Fact]
+		public void IsLL1()
+		{
+			Bnf bnf = TestLL1Bnf();
+			string error;
+			Assert.True(bnf.IsLL1(out error));
 
-            //New test with new bnf
-            Bnf bnf2 = TestLL1Bnf2();
-            Production S2 = bnf2.Productions.FirstOrDefault(p => p.Name == "S");
-            Symbol b2 = bnf2.Terminals.FirstOrDefault(s => s.Name == "b");
+			Bnf bnf2 = TestLL1Bnf2();
+			string bnf2Error;
+			Assert.True(bnf2.IsLL1(out bnf2Error));
 
-            List<Symbol> expected2 = new List<Symbol>() { b2 };
+			// based on the ll(2) grammar; expect false because of failure to comply with condition 2
+			Bnf bnf3 = TestLL2Bnf();
+			string bnf3Error;
+			Assert.False(bnf3.IsLL1(out bnf3Error));
+		}
 
-            Assert.Equal(expected2.Count, S2.FirstSet().Intersect(expected2).Count());
-        }
+		[Fact]
+		public void FirstSet()
+		{
+			Bnf bnf = TestLL1Bnf();
+			Production S = bnf.Productions.FirstOrDefault(p => p.Name == "S");
+			Symbol a = bnf.Terminals.FirstOrDefault(s => s.Name == "a");
+			Symbol b = bnf.Terminals.FirstOrDefault(s => s.Name == "b");
+			Symbol d = bnf.Terminals.FirstOrDefault(s => s.Name == "d");
+			List<Symbol> expected = new List<Symbol>() { a, b, d };
 
-        [Fact]
-        public void FollowSet()
-        {
-            Bnf bnf = TestLL1Bnf();
-            Production D = bnf.Productions.FirstOrDefault(p => p.Name == "D");
-            Symbol a = bnf.Terminals.FirstOrDefault(s => s.Name == "a");
-            List<Symbol> expected = new List<Symbol>() { a };
-            Assert.Equal(expected.Count, D.FollowSet(bnf).Intersect(expected).Count());
-        }
-    }
+			foreach (var item in S.FirstSet())
+			{
+				System.Diagnostics.Debug.WriteLine("expected" + " " + item);
+			}
+
+			Assert.Equal(expected.Count, S.FirstSet().Intersect(expected).Count());
+
+			//New test with new bnf
+			Bnf bnf2 = TestLL1Bnf2();
+			Production S2 = bnf2.Productions.FirstOrDefault(p => p.Name == "S");
+			Symbol b2 = bnf2.Terminals.FirstOrDefault(s => s.Name == "b");
+
+			List<Symbol> expected2 = new List<Symbol>() { b2 };
+
+			Assert.Equal(expected2.Count, S2.FirstSet().Intersect(expected2).Count());
+		}
+
+		[Fact]
+		public void FollowSet()
+		{
+			Bnf bnf = TestLL1Bnf();
+			Production D = bnf.Productions.FirstOrDefault(p => p.Name == "D");
+			Symbol a = bnf.Terminals.FirstOrDefault(s => s.Name == "a");
+			List<Symbol> expected = new List<Symbol>() { a };
+			Assert.Equal(expected.Count, D.FollowSet(bnf).Intersect(expected).Count());
+		}
+	}
 }
