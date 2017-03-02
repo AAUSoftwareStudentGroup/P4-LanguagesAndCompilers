77e3b01bf6bdd6d9a23708d17a872cca7b8bb379
Mathias Pihl <mathias@pihlerne.dk>
2017-03-02 14:49:38 +0100

    Almost finished tree-mess-remover

--- src/Compiler/Data/Visitor.cs
+++ src/Compiler/Data/Visitor.cs
@@ -9,8 +9,43 @@ namespace Compiler.Data
         public void Visit(Tree<T> ast)
         {
             VisitChildren(ast);
-            if((ast.Children.Count == 0 && !IsTerminal(ast)) || ast.Node.ToString() == "EPSILON")
+
+            if((ast.Children.Count == 0 && !IsTerminal(ast)) || ((ast.Node as AST_Node).name == "EPSILON")) 
+            {
                 RemoveSelfFromParent(ast);
+            } 
+            // If statementP's parent is statementP, put self into parent.parent.children
+            else if(ast.Parent != null && (
+                        (ast.Node as AST_Node).name == (ast.Parent.Node as AST_Node).name 
+                        || 
+                        (ast.Node as AST_Node).name == (ast.Parent.Node as AST_Node).name + "P"
+                   ))
+            {                
+                for(int i = 0; i < ast.Children.Count; i++) 
+                {
+                    ast.Parent.Children.Insert(ast.Parent.Children.IndexOf(ast), ast.Children[i]);
+                }
+                RemoveSelfFromParent(ast);
+            }
+            else if(ast.Children.Count == 1 && 
+                !IsTerminal(ast.Children[0]) &&  
+                ast.Children[0].Children.Count == 1 && 
+                ast.Parent != null) 
+            {
+                ast.Parent.Children.Insert(ast.Parent.Children.IndexOf(ast), ast.Children[0]);
+                RemoveSelfFromParent(ast);
+            }
+            else if((ast.Node as AST_Node).name == "NewLine") {
+                RemoveSelfFromParent(ast);
+            }
+            else if(ast.Children.Count > 0 &&
+                    (ast.Children[0].Node as AST_Node).name == "StartDel" 
+                    &&  
+                    (ast.Children[ast.Children.Count-1].Node as AST_Node).name == "EndDel"
+                ) {
+                ast.Children.RemoveAt(0);
+                ast.Children.RemoveAt(ast.Children.Count-1);
+            }
         }
         private void VisitChildren(Tree<T> ast) {
             for(int i = ast.Children.Count-1; i >= 0; i--) {
