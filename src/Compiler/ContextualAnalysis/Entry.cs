64cb46779adbb07e0da74f49561cfbcf648d92e7
Mathias Ibsen <mathias@ibsen.name>
2017-04-11 11:00:59 +0200

    Add initial structure for symbol tables git status

---
+++ src/Compiler/ContextualAnalysis/Entry.cs
@@ -0,0 +1,65 @@
+using Compiler.Data.AST;
+
+namespace Compiler.ContextualAnalysis
+{
+	public class Entry
+	{
+        private int _level;
+		public Entry(string id, Node node, int level)
+        {
+            this.Id = id;
+			this.Node = node;
+            this._level = level;
+        }
+
+        public int LevelNumber => _level;
+		public string Id
+		{
+			get;
+			set;
+		}
+
+        // modify when ast structure is done
+        public Node Node
+        {
+            get;
+            set;
+        }
+
+		public storageType StorageType
+		{
+			get;
+			set;
+		}
+
+		public Data.AST.Type Type
+		{
+			get;
+			set;
+		}
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Entry;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return Id.Equals(item.Id);
+        }
+
+	}
+
+	public enum storageType
+	{
+		valueType,
+		ReferenceType
+	}
+}
