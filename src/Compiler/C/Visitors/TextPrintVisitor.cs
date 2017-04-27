85630191b9b33555c60449d399ef4535215a03b6
Morten Rask <morten.rask@live.dk>
2017-04-27 14:12:08 +0200

    Fix TextPrintVisitor

--- src/Compiler/C/Visitors/TextPrintVisitor.cs
+++ src/Compiler/C/Visitors/TextPrintVisitor.cs
@@ -10,15 +10,34 @@ namespace Compiler.C.Visitors
     {
         public override IEnumerable<string> Visit(Node node)
         {
-            List<string> res = new List<string>();
+            StringBuilder strBuilder = new StringBuilder();
+
+            bool first = true;
+
             foreach (var child in node)
             {
                 foreach (var str in child.Accept(this))
                 {
-                    res.Add(str);
+                    if(!first)
+                    {
+                        strBuilder.Append(" ");
+                    }
+                    first = false;
+                    strBuilder.Append(str);
                 }
             }
-            yield return string.Join(" ", res);
+            yield return strBuilder.ToString();
+        }
+
+        public override IEnumerable<string> Visit(Data.C node)
+        {
+            foreach (var item in node)
+            {
+                foreach (var item2 in item.Accept(this))
+                {
+                    yield return item2;
+                }
+            }
         }
 
         public override IEnumerable<string> Visit(Token node)
@@ -29,138 +48,111 @@ namespace Compiler.C.Visitors
             }
         }
 
-        //public override IEnumerable<string> Visit(Data.C node)
-        //{
-        //    foreach (var item in node)
-        //    {
-        //        foreach (var item2 in item.Accept(this))
-        //        {
-        //            yield return item2;
-        //        }
-        //    }
-        //}
+        public override IEnumerable<string> Visit(Function node)
+        {
+            List<string> res = new List<string>();
+            foreach (var child in node.TakeWhile(n => n.Name != "{"))
+            {
+                foreach (var str in child.Accept(this))
+                {
+                    res.Add(str);
+                }
+            }
+            yield return string.Join(" ", res);
+            yield return "{";
+            foreach (var item in node.Nodes<Statement>()[0].Accept(this))
+            {
+                yield return $"    {item}";
+            }
+            yield return "}";
+        }
 
+        public override IEnumerable<string> Visit(CompoundStatement node)
+        {
+            foreach (var item in  node.Nodes<Statement>()[0].Accept(this))
+            {
+                yield return item;
+            }
+            foreach (var item in node.Nodes<Statement>()[1].Accept(this))
+            {
+                yield return item;
+            }
+        }
 
-        //public override IEnumerable<string> Visit(Token node)
-        //{
-        //    if(node.Value != "EPSILON")
-        //    {
-        //        yield return node.Value;
-        //    }
-        //}
+        public override IEnumerable<string> Visit(Statement node)
+        {
+            if(node.Count == 1)
+            {
+                return node.First().Accept(this);
+            }
+            else
+            {
+                return node.First().Accept(this).Select(s => s + ";");
+            }
+        }
 
-        //public override IEnumerable<string> Visit(Function node)
-        //{
-        //    List<string> res = new List<string>();
-        //    foreach (var child in node.TakeWhile(n => n.Name != "{"))
-        //    {
-        //        foreach (var str in child.Accept(this))
-        //        {
-        //            res.Add(str);
-        //        }
-        //    }
-        //    yield return string.Join(" ", res);
-        //    yield return "{";
-        //    foreach (var item in node.Nodes<Statement>()[0].Accept(this))
-        //    {
-        //        yield return $"    {item}";
-        //    }
-        //    yield return "}";
-        //}
+        public override IEnumerable<string> Visit(Functions node)
+        {
+            foreach (var item in node)
+            {
+                foreach (var item2 in item.Accept(this))
+                {
+                    yield return item2;
+                }
+            }
+        }
 
-        //public override IEnumerable<string> Visit(CompoundStatement node)
-        //{
-        //    foreach (var item in  node.Nodes<Statement>()[0].Accept(this))
-        //    {
-        //        yield return item;
-        //    }
-        //    foreach (var item in node.Nodes<Statement>()[1].Accept(this))
-        //    {
-        //        yield return item;
-        //    }
-        //}
+        public override IEnumerable<string> Visit(GlobalDeclarations node)
+        {
+            foreach (var item in node)
+            {
+                foreach (var item2 in item.Accept(this))
+                {
+                    yield return item2;
+                }
+            }
+        }
 
-        //public override IEnumerable<string> Visit(Statement node)
-        //{
-        //    foreach (var item in node)
-        //    {
-        //        var j = item.Accept(this).Where(s => s != ";");
-        //        if(j.Count() == 1)
-        //        {
-        //            yield return j.First() + ";";
-        //        }
-        //        else
-        //        {
-        //            foreach (var item2 in j)
-        //            {
-        //                yield return item2;
-        //            }
-        //        }
-        //    }
-        //}
+        public override IEnumerable<string> Visit(IfStatement node)
+        {
+            StringBuilder first = new StringBuilder("if(");
+            foreach (var str in node.Nodes<BooleanExpression>()[0].Accept(this))
+            {
+                first.Append(str);
+            }
+            first.Append(")");
+            yield return first.ToString();
+            yield return "{";
+            foreach (var str in node.Nodes<GlobalDeclarations>()[0].Accept(this))
+            {
+                yield return $"    {str}";
+            }
+            foreach (var str in node.Nodes<Statement>()[0].Accept(this))
+            {
+                yield return $"    {str}";
+            }
+            yield return "}";
+        }
 
-        //public override IEnumerable<string> Visit(Functions node)
-        //{
-        //    foreach (var item in node)
-        //    {
-        //        foreach (var item2 in item.Accept(this))
-        //        {
-        //            yield return item2;
-        //        }
-        //    }
-        //}
-
-        //public override IEnumerable<string> Visit(GlobalDeclarations node)
-        //{
-        //    foreach (var item in node)
-        //    {
-        //        foreach (var item2 in item.Accept(this))
-        //        {
-        //            yield return item2;
-        //        }
-        //    }
-        //}
-
-        //public override IEnumerable<string> Visit(IfStatement node)
-        //{
-        //    string first = "if(";
-        //    foreach (var str in node.Nodes<BooleanExpression>()[0].Accept(this))
-        //    {
-        //        first += str;
-        //    }
-        //    first += ")";
-        //    yield return first;
-        //    yield return "{";
-        //    foreach (var str in node.Nodes<GlobalDeclarations>()[0].Accept(this))
-        //    {
-        //        yield return $"    {str}";
-        //    }
-        //    foreach (var str in node.Nodes<Statement>()[0].Accept(this))
-        //    {
-        //        yield return $"    {str}";
-        //    }
-        //    yield return "}";
-        //}
-
-        //public override IEnumerable<string> Visit(WhileStatement node)
-        //{
-        //    string first = "while(";
-        //    foreach (var str in node.Nodes<BooleanExpression>()[0].Accept(this))
-        //    {
-        //        first += str;
-        //    }
-        //    first += ")";
-        //    yield return first;
-        //    yield return "{";
-        //    foreach (var str in node.Nodes<GlobalDeclarations>()[0].Accept(this))
-        //    {
-        //        yield return $"    {str}";
-        //    }
-        //    foreach (var str in node.Nodes<Statement>()[0].Accept(this))
-        //    {
-        //        yield return $"    {str}";
-        //    }
-        //    yield return "}";
-        //}
+        public override IEnumerable<string> Visit(WhileStatement node)
+        {
+            StringBuilder first = new StringBuilder("while(");
+            foreach (var str in node.Nodes<BooleanExpression>()[0].Accept(this))
+            {
+                first.Append(str);
+            }
+            first.Append(")");
+            yield return first.ToString();
+            yield return "{";
+            foreach (var str in node.Nodes<GlobalDeclarations>()[0].Accept(this))
+            {
+                yield return $"    {str}";
+            }
+            foreach (var str in node.Nodes<Statement>()[0].Accept(this))
+            {
+                yield return $"    {str}";
+            }
+            yield return "}";
+        }
     }
 }
