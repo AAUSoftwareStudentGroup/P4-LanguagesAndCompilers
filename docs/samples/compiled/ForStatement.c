9c37c45f1d04d4d0e054899b7ea3db8d0c39386c
Morten Rask <morten.rask@live.dk>
2017-04-28 15:52:20 +0200

    Fix statement order

--- docs/samples/compiled/ForStatement.c
+++ docs/samples/compiled/ForStatement.c
@@ -2,6 +2,7 @@ void main ( void ) ;
 long long int sum ;
 void main ( void )
 {
+    sum = 0;
     for( long long int x = 0 ; x <= 10 ; x ++ )
     {
         for( long long int y = 0 ; y <= x ; y ++ )
@@ -9,5 +10,4 @@ void main ( void )
             sum = ( sum + ( x * y ) );
         }
     }
-    sum = 0;
 }
