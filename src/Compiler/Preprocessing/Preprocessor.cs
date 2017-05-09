481c0020fab274a570ca186a68b3ed78883f8481
Mathias Pihl <mathias@pihlerne.dk>
2017-05-09 09:26:44 +0200

    Add import-functionality and blinkImport example of usage

---
+++ src/Compiler/Preprocessing/Preprocessor.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using Compiler.LexicalAnalysis;
+using System;
+using System.Collections.Generic;
+
+namespace Compiler.Preprocessing
+{
+	public class Preprocessor 
+	{
+		static private List<string> imports = new List<String>();
+        public  List<Token> Process(Lexer lexer, IEnumerable<Token> tokens) {
+			bool onlyImportsYet = true;
+			List<Token> newList = new List<Token>();
+			foreach(Token token in tokens) {
+				if(onlyImportsYet && token.Name == "import" && !imports.Contains(token.Value)) {
+					var newTokens = Process(lexer,lexer.Analyse(File.ReadAllText("../../docs/samples/" + token.Value.Replace("import ", "") + ".tang")));
+					if(newTokens == null)
+						throw new Exception("Import not found");
+					foreach(var newToken in newTokens)
+						if(newToken.Name != "eof")
+							newList.Add(newToken);
+						else
+							newList.RemoveAt(newList.Count - 1); // To remove last inserted newline
+					imports.Add(token.Value);
+				}
+				else if(token.Name == "newline") {
+					// Yield but dont set onlyImportsYet
+					newList.Add(token);
+				} else if(onlyImportsYet == false && token.Name == "import") {
+					throw new Exception("Imports must be at start of file");
+				} else if(token.Name == "import" && imports.Contains(token.Value)) {
+					// Do exactly nothing. We already imported this
+				} else {
+					onlyImportsYet = false;
+					newList.Add(token);
+				}
+			}
+			return newList;
+		}
+    }
+}
