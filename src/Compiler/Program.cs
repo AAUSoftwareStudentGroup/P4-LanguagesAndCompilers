8063209b89e030bbe5d2f2cdac8d37ebceb3f82b
Morten Rask <morten.rask@live.dk>
2017-05-10 15:54:40 +0200

    Fix empty program compilation

--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -16,7 +16,7 @@ namespace Compiler
             Lexer lexer = new Lexer(args.Length == 3 ? args[2] : "../../docs/tang.tokens.json");
             bool DebugEnabled = false;
 
-            string file = "../../docs/samples/test2.tang";
+            string file = "../../docs/samples/Empty.tang";
 
             if(args.Length > 0)
             {
