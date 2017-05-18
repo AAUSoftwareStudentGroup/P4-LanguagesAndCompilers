06aa49837dd83428f89fb5565f21e6db5f92429f
Mathias Pihl <mathias@pihlerne.dk>
2017-05-18 14:00:48 +0200

    Burns the witch

--- src/Compiler.Tests/Testfiles/tang/CompareDeclAndAssTwoLinesCorrectly.tang.c
+++
@@ -1,8 +0,0 @@
-signed int a ;
-int Pow ( signed long a , unsigned long b ) ;
-void main ( ) ;
-int Pow ( signed long a , unsigned long b ) { signed long r = 1 ; for ( unsigned long i = 0 ; i < b ; i ++ ) { r *= a ; } return r ; }
-void main ( )
-{
-    a = 27 ;
-}
