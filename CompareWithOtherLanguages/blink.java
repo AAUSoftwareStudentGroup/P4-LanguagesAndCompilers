7abec788e5f4087388cb48881c594702d7f1246d
Mathias Pihl <mathias@pihlerne.dk>
2017-05-19 18:49:08 +0200

    Recompile other languages into .hex

--- CompareWithOtherLanguages/blink.java
+++ CompareWithOtherLanguages/blink.java
@@ -3,19 +3,19 @@ package arduino.tutorial;
 import static haiku.avr.lib.arduino.WProgram.*;
 import static haiku.avr.AVRConstants.*;
 
-public class Blink {
+public class blink {
 
-	public static void wait() {
-		for(long long i = 0; i < 3200000; i++) {}
+	public static void _wait() {
+		for(long i = 0; i < 3200000; i++) {}
 	}
 
 	public static void main(String[] args) {
 		DDRB |= 1<<5;
 		while(true) {
 			PORTB |= 1<<5;             // Turn LED on
-			wait();                    // waits for a second
+			_wait();                    // waits for a second
 			PORTB &= ~(1<<5);          // Turn LED of
-			wait();                    // waits for a second
+			_wait();                    // waits for a second
 		}
 	}
 }
