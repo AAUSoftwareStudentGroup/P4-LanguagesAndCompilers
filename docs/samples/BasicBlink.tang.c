b3158b22c7721f71fe73d3ac356cf60c2c2acece
Morten Rask <morten.rask@live.dk>
2017-05-26 12:54:01 +0200

    Add compileable test tang file in Compiler folder

--- docs/samples/BasicBlink.tang.c
+++ docs/samples/BasicBlink.tang.c
@@ -1,26 +1,12 @@
-volatile unsigned char * ddrb ;
-volatile unsigned char * port ;
-signed long counter ;
+signed char h ( signed char a , signed char b ) ;
 signed long Pow ( signed long a , signed long b ) ;
 void main ( ) ;
 signed long Pow ( signed long a , signed long b ) { signed long r = 1 , i ; for ( i = 0 ; i < b ; i ++ ) { r *= a ; } return r ; }
+signed char h ( signed char a , signed char b )
+{
+    return ( a + b ) ;
+}
 void main ( )
 {
-    ddrb = ( volatile unsigned char * ) ( 36 ) ;
-    port = ( volatile unsigned char * ) ( 37 ) ;
-    * ddrb = ( 1 ? ( ( * ddrb ) | 1 << ( 5 ) ) : ( ( * ddrb ) & ~ ( 1 << ( 5 ) ) ) ) ;
-    * port = ( 1 ? ( ( * port ) | 1 << ( 5 ) ) : ( ( * port ) & ~ ( 1 << ( 5 ) ) ) ) ;
-    counter = 0 ;
-    while ( 1 )
-    {
-        if ( ( counter == 100000 ) )
-        {
-            counter = 0 ;
-            * port = ( ( ! ( * port & ( 1 << ( 5 ) ) ) ) ? ( ( * port ) | 1 << ( 5 ) ) : ( ( * port ) & ~ ( 1 << ( 5 ) ) ) ) ;
-        }
-        else
-        {
-            counter = ( counter + 1 ) ;
-        }
-    }
+    h ( 100 , h ( 100 , 100 ) ) ;
 }
